787a771ec3808e83be87ac17e184085490e2fa2e
AnderP <andersoncordovapp@gmail.com>
2025-06-15 12:14:46 -0500

    Add funtions resta multiplicacion division

--- calculator.py
+++ calculator.py
@@ -10,6 +10,33 @@ def suma():
     except ValueError:
         messagebox.showerror("Error", "¡Ingresa solo números!")
 
+def resta():
+    try:
+        num1 = float(entrada_num1.get())
+        num2 = float(entrada_num2.get())
+        resultado = num1 - num2
+        etiqueta_resultado.config(text=f"Resultado: {resultado}")
+    except ValueError:
+               messagebox.showerror("Error", "¡Ingresa solo números!")
+
+def multiplicacion():
+    try:
+        num1 = float(entrada_num1.get())
+        num2 = float(entrada_num2.get())
+        resultado = num1 * num2
+        etiqueta_resultado.config(text=f"Resultado: {resultado}")
+    except ValueError:
+               messagebox.showerror("Error", "¡Ingresa solo números!")
+
+def division():
+    try:
+        num1 = float(entrada_num1.get())
+        num2 = float(entrada_num2.get())
+        resultado = num1 / num2
+        etiqueta_resultado.config(text=f"Resultado: {resultado}")
+    except ValueError:
+               messagebox.showerror("Error", "¡Ingresa solo números!")
+
 # Configuración de la ventana
 ventana = tk.Tk()
 ventana.title("Calculadora Basica")
@@ -27,6 +54,18 @@ entrada_num2.pack()
 # Botón para sumar
 boton_sumar = tk.Button(ventana, text="Sumar", command=suma)
 boton_sumar.pack(pady=10)
+
+# Botón para restar
+boton_restar = tk.Button(ventana, text="Restar", command=resta)
+boton_restar.pack(pady=10)
+
+# Botón para multiplicar
+boton_restar = tk.Button(ventana, text="Multiplicar", command=multiplicacion)
+boton_restar.pack(pady=10)
+
+# Botón para dividir
+boton_restar = tk.Button(ventana, text="Dividir", command=division)
+boton_restar.pack(pady=10)
 
 # Etiqueta para mostrar el resultado
 etiqueta_resultado = tk.Label(ventana, text="Resultado: ")
